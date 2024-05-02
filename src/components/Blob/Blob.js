5d461567164ddb1db42b6a59368b422c758f4afb
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2024-05-02 18:25:48 +0100

    BCT-7: Enhanced Blob component with click interactivity and updated styles

--- src/components/Blob/Blob.js
+++ src/components/Blob/Blob.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Blob.css';
+import { ref, set, onValue } from "firebase/database";
+import { database } from '../Firebase/FireBase';
 
 function Blob() {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const countRef = ref(database, 'clicks/count');
+    onValue(countRef, (snapshot) => {
+      const data = snapshot.val();
+      setCount(data || 0);
+    });
+  }, []);
+
+  const handleClick = () => {
+    const newCount = count + 1;
+    set(ref(database, 'clicks/count'), newCount);
+  };
+
   return (
-    <div className="Blob">
-      <p>Click me!</p>
+    <div className="Blob" onClick={handleClick}>
+      <p>Click count: {count}</p>
     </div>
   );
 }
