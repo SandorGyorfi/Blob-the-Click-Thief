ec8f1ceb8f2ccb8084e7b0fdaeef6653bc38070d
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2024-05-02 18:22:21 +0100

    BCT-2: Set up basic React components and styling for Blob and ClickPanel

---
+++ src/components/Blob/Blob.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blob.css';
+
+function Blob() {
+  return (
+    <div className="Blob">
+      <p>Click me!</p>
+    </div>
+  );
+}
+
+export default Blob;
---
+++ src/components/ClickPanel/ClickPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ClickPanel.css';
+
+function ClickPanel() {
+  return (
+    <div className="ClickPanel">
+      <p>Panel for click messages will appear here.</p>
+    </div>
+  );
+}
+
+export default ClickPanel;
